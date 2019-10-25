120fea17eef19179e4de5e57e5aef9cc9835fdc7
neikist <neikist1992@gmail.com>
2019-10-25 15:02:19 +0300

    Добавил гредл таску для подписи apk

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.*
+
 plugins {
     id("base")
     id("com.novoda.build-properties") version "0.4.1"
@@ -17,6 +19,15 @@ val apkName = fileSrcApk.nameWithoutExtension
 
 val androidSdkDir = localProperties["sdk.dir"]?.string ?: error("Need define path to android sdk")
 
+val keystorePath = localProperties["keystore.path"]?.string ?: error("Need define path to keystore")
+val keystorePassword = localProperties["keystore.password"]?.string ?: error("Need define keystore password")
+val keyAlias = localProperties["key.alias"]?.string ?: error("Need define key alias")
+val keyPassword = localProperties["key.password"]?.string ?: error("Need define key password")
+
+val buildToolsVersion = localProperties["buildToolsVersion"]?.string ?: error("Need define sdk build tools version")
+
+val runOnWindows = runOnWindows()
+
 tasks {
     task("print") {
         doLast {
@@ -25,19 +36,18 @@ tasks {
     }
 
     val apktoolUnpackDestinationDir = "${layout.buildDirectory.get().asFile.path}${File.separator}unpacked"
-    val unpack = task("unpack", type=Exec::class) {
-        doFirst{
-            // linux
-            commandLine("apktool")
-
-            // windows
-            commandLine("PowerShell", "apktool.bat")
+    val unpack = task("unpack", type = Exec::class) {
+        doFirst {
+            if (runOnWindows) {
+                commandLine("PowerShell", "apktool.bat")
+            } else {
+                commandLine("apktool")
+            }
             args("-f", "d $apkFilePath", "-o $apktoolUnpackDestinationDir")
-
         }
     }
 
-    val copySmaliFiles = task("copySmaliFiles", type=Copy::class){
+    val copySmaliFiles = task("copySmaliFiles", type = Copy::class) {
 
         from(layout.projectDirectory.dir("src/smali").asFile.path) {
             include("**/*.smali")
@@ -47,15 +57,39 @@ tasks {
     }
 
     val apktoolPackDestinationDir = "${layout.buildDirectory.get().asFile.path}${File.separator}$apkName.apk"
-    val pack = task("pack", type=Exec::class) {
+    val pack = task("pack", type = Exec::class) {
         dependsOn(copySmaliFiles)
 
-        // linux
-        commandLine("apktool")
+        if (runOnWindows) {
+            commandLine("PowerShell", "apktool.bat")
+        } else {
+            commandLine("apktool")
+        }
 
-        // windows
-        commandLine("PowerShell", "apktool.bat")
         args("b $apktoolUnpackDestinationDir", "-o $apktoolPackDestinationDir")
 
     }
+
+    val sign = task("sign", type = Exec::class) {
+        dependsOn(pack)
+        val separator = File.separator
+        if (runOnWindows) {
+            commandLine(
+                    "PowerShell",
+                    "${androidSdkDir}${separator}build-tools$separator$buildToolsVersion${separator}apksigner.bat sign"
+            )
+        } else {
+            commandLine("${androidSdkDir}${separator}build-tools$separator$buildToolsVersion${separator}apksigner sign")
+        }
+
+        args(" --ks $keystorePath ",
+                " --ks-key-alias $keyAlias ",
+                " --ks-pass $keystorePassword ",
+                " --key-pass $keyPassword ",
+                apktoolPackDestinationDir)
+    }
+
+    assemble.get().dependsOn(sign)
 }
+
+fun runOnWindows(): Boolean = System.getProperty("os.name").toLowerCase(Locale.ROOT).contains("windows")
